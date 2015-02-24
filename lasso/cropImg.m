67f6fd7267d41741394ee8d1d19b2724c1f5e039
stevenoh93 <stevenoh93@gmail.com>
2015-02-24 17:14:59 -0500

    activity log update

--- lasso/cropImg.m
+++ lasso/cropImg.m
@@ -1,9 +1,9 @@
 %% Read images with prefixes of certain format.
 %  Returns cells each containing b/w images with edge detection
 
-imgPre = 'obj';
+imgPre = 'img360';
 imgType = '.jpg';
-N = 14;
+N = 18;
 fudge = 1;
 xoff = 150;
 yoff = 500;
@@ -14,7 +14,9 @@ for ii=1:N
     img = imread([imgPre num2str(ii) imgType]); % Read image
 %     [xs ys zs] = size(img);
 %     img = img(xoff:xs-xoff,yoff:ys-yoff,2);              % Crop/To b/w
-    [~,thresh] = edge(img,'sobel');             % Edge detect and save
-    bws = edge(img,'sobel',thresh*fudge);
-    images{ii} = bws;
+%     img = img(:,:,2);
+%     [~,thresh] = edge(img,'sobel');             % Edge detect and save
+%     bws = edge(img,'sobel',thresh*fudge);
+%     images{ii} = bws;
+    images{ii} = img;
 end
