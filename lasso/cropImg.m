bcffd6be6b436c62edf0988ce4fff1542b72867c
stevenoh93 <stevenoh93@gmail.com>
2015-02-24 16:46:27 -0500

    New image set with 360 degree rotation

--- lasso/cropImg.m
+++ lasso/cropImg.m
@@ -5,12 +5,15 @@ imgPre = 'obj';
 imgType = '.jpg';
 N = 14;
 fudge = 1;
+xoff = 150;
+yoff = 500;
 
 images = cell(1,N);
 
 for ii=1:N
     img = imread([imgPre num2str(ii) imgType]); % Read image
-    img = img(300:900,550:1300,2);              % Crop/To b/w
+%     [xs ys zs] = size(img);
+%     img = img(xoff:xs-xoff,yoff:ys-yoff,2);              % Crop/To b/w
     [~,thresh] = edge(img,'sobel');             % Edge detect and save
     bws = edge(img,'sobel',thresh*fudge);
     images{ii} = bws;
