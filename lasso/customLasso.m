43cc12577ae530179c914c6127f34d66b4b08b76
stevenoh93 <stevenoh93@gmail.com>
2015-03-07 19:43:31 -0500

    Tried lasso with color image and euclidean dist. Doesnt work

--- lasso/customLasso.m
+++ lasso/customLasso.m
@@ -29,6 +29,8 @@ centery = 0;
 [centerx centery key] = ginput(1);
 hold on;
 scatter(centerx,centery,minsize/20,'x','b');
+disp(['centerx = ' num2str(centerx)]); %(917, 393)
+disp(['centery = ' num2str(centery)]);
 
 disp('Now, drag around the object as close as possible');
 set(fig,'WindowButtonDownFcn',@buttonDown);
@@ -49,57 +51,48 @@ function buttonDown(object, event)
 function buttonMotion(object, event)
     global centerx;
     global centery;
-    global img
-    global minsize
-    thresh = 3;
+    global img;
+    global minsize;
+    thresh = 20;
     s = size(img);
     disp(s);
 
     C = get(gca,'CurrentPoint');
-    cp = [C(3) C(1)];
-    if(abs(cp(1)) < 1)
-        dy = 0;
-    else
-        dy = cp(1)-centery;
-    end
-    if(abs(cp(2)) < 1)
-        dx = 0;
-    else
-        dx = cp(2)-centerx;
-    end
-    
-    mind = min(dx,dy);
-    dir = [round(dy/mind) round(dx/mind)];
-    
-    disp(['dx = ' num2str(dir(2))]);
-    disp(['dy = ' num2str(dir(1))]);
-    np = cp;
+    np = [C(3) C(1)];
     while 1
         cp = np;
-        if(abs(cp(1)) < 1)
+        % get new direction towards the center
+        if(abs(cp(1)-centery) < 1)
             dy = 0;
         else
             dy = cp(1)-centery;
         end
-        if(abs(cp(2)) < 1)
+        if(abs(cp(2)-centerx) < 1)
             dx = 0;
         else
             dx = cp(2)-centerx;
         end
-        mind = min(dx,dy);
+        mind = -1*abs(min(dx,dy));
+        if(mind == 0)
+            mind = -1*abs(max(dx,dy));
+        end
         dir = [round(dy/mind) round(dx/mind)];
-        np = cp + dir*10;
+        np = cp + dir*5;
+%         disp(['mind = ' num2str(mind)]);
+%         disp(['dx = ' num2str(dir(2))]);
+%         disp(['dy = ' num2str(dir(1))]);
         % Compare pixel values between cp and np
-%         ind1 = sub2ind(s,np(1),np(2));
-%         ind2 = sub2ind(s,cp(1),cp(2));
-%         disp(class(ind1));
-        contr = abs(img(round(sub2ind(s,np(1),np(2)))) - img(round(sub2ind(s,cp(1),cp(2)))));
-        dist = sum((np-[centery centerx])*(np-[centery centerx])');
-        disp(['contr = ' num2str(contr)]);
-        disp(['dst = ' num2str(dist)]);
+        contr = getDiffMetric(cp,np);
+        % Get physical distance between cp and np
+        dist = euclideanDist(np,[centery centerx]);
+        
+%         disp(['dst = ' num2str(dist)]);
         if(contr > thresh)
             hold on;
-            scatter(cp(2)+dir(2),cp(1)+dir(1),minsize/25,'o');
+            selectPoint([cp(2)+dir(2), cp(1)+dir(1)],minsize/50);
+            disp(['contr = ' num2str(contr)]);
+            disp(cp);
+            disp(np);
             break;
         end
         if(dist < 3000)
@@ -114,3 +107,17 @@ function buttonUp(object, event)
     % get the properties and restore them
     props = getappdata(object,'TestGuiCallbacks');
     set(object,props);
+    
+function ret = getDiffMetric(currentPoint, nextPoint)
+    global img;
+    pix1 = img(round(sub2ind(size(img),nextPoint(1),nextPoint(2))));
+    pix2 = img(round(sub2ind(size(img),currentPoint(1),currentPoint(2))));
+    % for grayscale
+%     ret = abs(pix1 - pix2);
+    % for rgb
+    ret = euclideanDist(pix1, pix2);
+function dist = euclideanDist(p1, p2)
+    dist = sum((p1-p2)*(p1-p2)');
+    
+function selectPoint(point,pointSize)
+    scatter(point(1),point(2),pointSize,'o','b');
